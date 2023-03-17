9ad30f355ff9a15c8837e164ca5dee1df3985676
artemkuskin <artemkuskin7@gmail.com>
2023-03-17 17:52:16 +0300

    refactor: add style

--- components/Component1.tsx
+++ components/Component1.tsx
@@ -3,14 +3,16 @@ export const Component1 = () => {
   return (
     <div className={styles.component}>
       <div className={styles.videoInner}>
-        <video
-          className={styles.video}
-          src="https://wisr.com.au/assets/media/smart-part-journey/00_intro.mp4"
-          poster="https://wisr.com.au/assets/media/smart-part-journey/00_intro.jpg"
-          autoPlay
-          muted
-        ></video>
-        <div className={styles.contentComponent1}></div>
+        <div className={styles.popup}>
+          <video
+            className={styles.video}
+            src="https://wisr.com.au/assets/media/smart-part-journey/00_intro.mp4"
+            poster="https://wisr.com.au/assets/media/smart-part-journey/00_intro.jpg"
+            autoPlay
+            muted
+          ></video>
+          <div className={styles.contentComponent1}></div>
+        </div>
       </div>
     </div>
   );
