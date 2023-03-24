a07329c7e16a6468ceee884dcf5739ce85d654b2
artemkuskin <artemkuskin7@gmail.com>
2023-03-24 12:56:03 +0300

    creatw animation content component2

--- components/Component2.tsx
+++ components/Component2.tsx
@@ -1,6 +1,17 @@
 import styles from "@/styles/Video.module.css";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export const Component2 = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      delay: 100,
+      once: true,
+    });
+  }, []);
+
   return (
     <div className={styles.contantVideo}>
       <div className={styles.videoInner}>
@@ -11,7 +22,39 @@ export const Component2 = () => {
           autoPlay
           muted
         ></video>
-        <div className={styles.contentComponent2}></div>
+        <div className={styles.contentComponent2}>
+          <h2>
+            <div className={styles.animComponent2} data-aos="fade-up" data-aos-once="true">
+              <div className={styles.component2Line}>SMART</div>
+              <div className={styles.component2Line}>HABITS</div>
+            </div>
+            <div className={styles.animComponent2} data-aos="fade-up" data-aos-once="true">
+              <div className={styles.component2Line}>HELP</div>
+              <div className={styles.component2Line}>YOU</div>
+              <div className={styles.component2Line}>SAVE</div>
+            </div>
+          </h2>
+          <div>
+            <div className={styles.animComponent2} data-aos="fade-up" data-aos-once="true">
+              <div className={styles.component2Line}>Use</div>
+              <div className={styles.component2Line}>the</div>
+              <div className={styles.component2Line}>science</div>
+              <div className={styles.component2Line}>of</div>
+              <div className={styles.component2Line}>habit</div>
+              <div className={styles.component2Line}>change</div>
+            </div>
+            <div className={styles.animComponent2} data-aos="fade-up" data-aos-once="true">
+              <div className={styles.component2Line}>to</div>
+              <div className={styles.component2Line}>achieve</div>
+              <div className={styles.component2Line}>your</div>
+              <div className={styles.component2Line}>money</div>
+              <div className={styles.component2Line}>goals</div>
+            </div>
+          </div>
+          <button className={styles.component2Button} data-aos="fade-up" data-aos-once="true">
+            GO TO WISR TODAY
+          </button>
+        </div>
       </div>
     </div>
   );
--- components/Component3.tsx
+++ components/Component3.tsx
@@ -1,5 +1,15 @@
 import styles from "@/styles/Video.module.css";
+import Aos from "aos";
+import { useEffect } from "react";
 export const Component3 = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+      delay: 100,
+      once: true,
+    });
+  }, []);
+
   return (
     <div className={styles.contantVideo}>
       <div className={styles.videoInner}>
@@ -10,7 +20,35 @@ export const Component3 = () => {
           autoPlay
           muted
         ></video>
-        <div className={styles.contentComponent3}></div>
+        <div className={styles.contentComponent3}>
+          <h2>
+            <div className={styles.animComponent2} data-aos="fade-up" data-aos-once="true">
+              <div className={styles.component2Line}>PERSONAL</div>
+              <div className={styles.component2Line}>LOANS</div>
+            </div>
+            <div className={styles.animComponent2} data-aos="fade-up" data-aos-once="true">
+              <div className={styles.component2Line}>SHOULD</div>
+              <div className={styles.component2Line}>HAVE</div>
+              <div className={styles.component2Line}>A</div>
+            </div>
+            <div className={styles.animComponent2} data-aos="fade-up" data-aos-once="true">
+              <div className={styles.component2Line}>PERSONAL</div>
+              <div className={styles.component2Line}>RATE</div>
+            </div>
+          </h2>
+          <div>
+            <div className={styles.animComponent2} data-aos="fade-up" data-aos-once="true">
+              <div className={styles.component2Line}>Based</div>
+              <div className={styles.component2Line}>on</div>
+              <div className={styles.component2Line}>your</div>
+            </div>
+            <div className={styles.animComponent2} data-aos="fade-up" data-aos-once="true">
+              <div className={styles.component2Line}>unique</div>
+              <div className={styles.component2Line}>credit</div>
+              <div className={styles.component2Line}>score</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
